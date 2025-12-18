69efea87ba4cecee7207141d017ad0efb6062e7f
Nikita <remnovmikita@gmail.com>
2025-12-18 16:00:02 +0200

    fix bug

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,15 +1,16 @@
- import axios from 'axios';
- export default async function fetchData(query) {
-    const response = await axios.get('https://pixabay.com/api/',{
-    params: {
-    key: '53507836-a90b7328b368e53f321449aea',
-    q: query,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    },
-});
-return response.data;
- }
- 
+import axios from "axios";
 
+const BASE_URL = "https://pixabay.com/api/";
+const API_KEY = '53507836-a90b7328b368e53f321449aea';
+
+export function apiFuncs(query){
+    return  axios.get(BASE_URL, {
+            params:{
+            key: API_KEY,
+            q: query,
+            image_type: "photo",
+            orientation: 'horizontal',
+            safesearch: true,
+            }
+        })
+}
--- src/main.js
+++ src/main.js
@@ -1,54 +1,48 @@
-import fetchData from './js/pixabay-api';
-import photoSetings from './js/render-functions';
-/////////////////////////////////////////////////////
+const form = document.querySelector(".form");
+const input = document.querySelector('[name="search-text"]');
+const span = document.querySelector('.loader');
+
 // Описаний у документації
 import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
-/////////////////////////////////////////////////////
-const load =document.querySelector('.loader');
-const form = document.querySelector('.form');
-const input = document.querySelector('[name=search-text]');
-const btn = document.querySelector('.form button');
-const gallary = document.querySelector('.gallery');
-load.style.display = 'none';
 
-form.addEventListener("submit", async (event) => {
-  event.preventDefault();
-  const nameForPhoto = input.value.trim();
-  if (nameForPhoto === "") {
-    iziToast.error({
-      title: "Error",
-      message: "Введите запрос для поиска!",
-    });
-    return;
-  }
+// Import first function
+import { apiFuncs } from "./js/pixabay-api";
+import { photo } from "./js/render-functions";
 
-  load.style.display = 'block'; // показать спиннер
-  const startTime = Date.now(); // запоминаем время старта
-
-  try {
-    const data = await fetchData(nameForPhoto);
-    if (data.totalHits === 0) {
-      iziToast.error({
-        title: "Error",
-        message: "По вашему запросу ничего не найдено!",
-      });
-      return;
+form.addEventListener("submit", even =>{
+    even.preventDefault();
+   const searcher = input.value.trim();
+   span.classList.add('show');
+    if(searcher === ""){
+        span.classList.remove("show");
+        iziToast.error({
+            title:'Eror',
+            message: 'Sorry, there are no images matching your search query. Please try again!'
+        });
     }
-    gallary.innerHTML = "";
-    photoSetings(gallary, data, input);
-  } catch (error) {
-    iziToast.error({
-      title: "Error",
-      message: "Ошибка запроса: " + error,
-    });
-  } finally {
-    const elapsed = Date.now() - startTime; // сколько прошло
-    const delay = Math.max(0, 400 - elapsed); // если прошло меньше 0.5 сек — ждём остаток
-    setTimeout(() => {
-      load.style.display = 'none';
-    }, delay);
-  }
-});
-
+    else{
+        const gallarey = document.querySelector(".gallery");
+            gallarey.innerHTML = "";
+       apiFuncs(searcher)
+        .then(resp => {
+            setTimeout (() =>{
+            if (resp.data.total === 0){
+                span.classList.remove("show");
+             iziToast.error({
+            title:'Eror',
+            message: 'Sorry, there are no images matching your search query. Please try again!'
+            });
+            return;   
+            }
+                console.log(resp.data);
+                photo(resp.data.hits, gallarey);
+                span.classList.remove("show");
+            }, 2000);
+           
+              
+        })
+        .catch(error => console.log(error) )
+    }
+})
